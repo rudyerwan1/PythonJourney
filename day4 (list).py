f52c4182113d289a5cda983dc77046ae4b173c95
erwan <erwan@erwans-Mac-mini.local>
2025-02-25 04:33:00 -0800

    First commit

---
+++ day4 (list).py
@@ -0,0 +1,116 @@
+
+import random
+"""
+Friends = ["chapie", "russel", "roslin", "amadou" , "brice" , "donaold"]
+#Friends = ["chapie", "russel", "roslin", "amadou" , "brice" , "donaold"]
+
+print(random.choice(Friends))
+"""
+#rock paper ciseau rock
+#user_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n")
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n"))
+choice = ["Rock", "Paper", "Scissors"]
+computer_choice = random.choice(choice)
+if user_choice == 0:
+    if computer_choice == "Rock":
+        print("""
+            _______
+        ---'   ____)
+              (_____)
+              (_____)
+              (____)
+        ---.__(___)
+        """)
+        print(f"Computer choose {computer_choice} Match nul")
+    elif computer_choice == "Paper":
+        # Paper
+        print("""
+             _______
+        ---'    ____)____
+                   ______)
+                  _______)
+                 _______)
+        ---.__________)
+        """)
+
+        print( f"Computer choose {computer_choice} you win")
+    else:
+        print("""
+            _______
+        ---'   ____)____
+                  ______)
+               __________)
+              (____)
+        ---.__(___)
+        """)
+        print(f"computer choose {computer_choice} you loose")
+
+elif user_choice == 1 :
+    if computer_choice == "Rock":
+        print("""
+            _______
+        ---'   ____)
+              (_____)
+              (_____)
+              (____)
+        ---.__(___)
+        """)
+        print(f"Computer choose {computer_choice} You win")
+    elif computer_choice == "Paper":
+        # Paper
+        print("""
+             _______
+        ---'    ____)____
+                   ______)
+                  _______)
+                 _______)
+        ---.__________)
+        """)
+
+        print( f"Computer choose {computer_choice} Match nul")
+    else:
+        print("""
+            _______
+        ---'   ____)____
+                  ______)
+               __________)
+              (____)
+        ---.__(___)
+        """)
+        print(f"computer choose {computer_choice} you loose")
+
+elif user_choice == 2 :
+    if computer_choice == "Rock":
+        print("""
+            _______
+        ---'   ____)
+              (_____)
+              (_____)
+              (____)
+        ---.__(___)
+        """)
+        print(f"Computer choose {computer_choice} You loose")
+    elif computer_choice == "Paper":
+        # Paper
+        print("""
+             _______
+        ---'    ____)____
+                   ______)
+                  _______)
+                 _______)
+        ---.__________)
+        """)
+
+        print( f"Computer choose {computer_choice} You Win")
+    else:
+        print("""
+            _______
+        ---'   ____)____
+                  ______)
+               __________)
+              (____)
+        ---.__(___)
+        """)
+        print(f"computer choose {computer_choice} Match nul")
+else :
+    print("Wrong select, Type 0 for Rock, 1 for Paper or 2 for Scissors\n")
