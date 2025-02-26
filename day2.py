6563b6448b2302cc2b0dc44c001690444db06eca
rudyerwan1 <rudymboda4@gmail.com>
2025-02-26 00:35:43 -0800

    revision des exercices

--- day2.py
+++ day2.py
@@ -1,7 +1,16 @@
 #Tips calculator
-print(" Hey body Let me be your next tips calculator")
-bill_amount= float(input("What is the total bill? \n"))
-tips_amount =int(input("How much tips would you like to give ? 10,12,15 \n"))
-people_shared = int(input("How many people will shared the bill?"))
+print(" Hey buddy my name is layana let me be your next tips calculator")
 
-total_to_pay = bill_amount
+bill_amount= float(input("What is the total bill? \n"))
+
+tips_amount =int(input("How much tips would you like to give ? 10,12,15 \n"))
+if tips_amount == 15 :
+    print("Waouh you so generous!")
+else :
+    print("Thank you buddy ")
+people_shared = int(input("How many people will shared the bill?\n"))
+
+total_to_pay =  (bill_amount * (1+ (tips_amount/100))) / people_shared
+
+#remeber how to around float number with f string class
+print(f"Each person would have to pay $ {total_to_pay:.2f}")
