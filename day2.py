f9162ec11dd250c500d0fd216867d0fd99a06296
rudyerwan1 <rudymboda4@gmail.com>
2025-02-26 00:23:16 -0800

    revision des exercices

---
+++ day2.py
@@ -0,0 +1,7 @@
+#Tips calculator
+print(" Hey body Let me be your next tips calculator")
+bill_amount= float(input("What is the total bill? \n"))
+tips_amount =int(input("How much tips would you like to give ? 10,12,15 \n"))
+people_shared = int(input("How many people will shared the bill?"))
+
+total_to_pay = bill_amount
