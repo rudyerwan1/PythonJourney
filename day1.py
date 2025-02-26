4dd7b1771ce9fa4b497b1427010462367e77d487
rudyerwan1 <rudymboda4@gmail.com>
2025-02-26 00:15:04 -0800

    ajout des fichiers de suivis

---
+++ day1.py
@@ -0,0 +1,7 @@
+# Band Name generator
+print("Let's guess your next band name ")
+city_name= input("In which city did you grew up?\n")
+car_name = input("What was your first car ?\n")
+
+print(f"Ok body your next name could be {city_name} {car_name}")
+
